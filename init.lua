e06ddc3f0b1b01d31a912029660088ffa5f8bb54
Bimbok <bimbokmkj@gmail.com>
2025-10-12 10:44:31 +0530

    terminal-is-removed

--- init.lua
+++ init.lua
@@ -15,18 +15,6 @@ function RunCurrentFile()
     cmd = string.format("gcc '%s' -o /tmp/%s && /tmp/%s", file, filename, filename)
   elseif ext == "cpp" then
     cmd = string.format("g++ '%s' -o /tmp/%s && /tmp/%s", file, filename, filename)
-  elseif ext == "java" then
-    -- Check for Maven project
-    local pom_path = vim.fn.findfile("pom.xml", ".;")
-    if pom_path ~= "" then
-      local main_class = "com.example.App" -- Default maven class
-      cmd = string.format("mvn compile exec:java -Dexec.mainClass=%s", main_class)
-    else
-      -- Handle plain IntelliJ-style project: compile src/*.java, run Main
-      local src_dir = vim.fn.getcwd() .. "/src"
-      local main_class = filename -- assumes current file has main()
-      cmd = string.format("javac %s/*.java && java -cp %s %s", src_dir, src_dir, main_class)
-    end
   elseif ext == "py" then
     cmd = string.format("python3 '%s'", file)
   else
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,43 +7,11 @@ vim.keymap.set("n", "<leader>ee", function()
   vim.cmd("Neotree toggle right")
 end, { desc = "Toggle Neo-tree" })
 
--- Open Terminal Horizontal by <leader>jj
-local terminal_buf = nil
-local terminal_win = nil
-vim.keymap.set("n", "<leader>`", function()
-  if
-    terminal_buf
-    and vim.api.nvim_buf_is_valid(terminal_buf)
-    and terminal_win
-    and vim.api.nvim_win_is_valid(terminal_win)
-  then
-    -- Terminal open, close it
-    vim.api.nvim_win_close(terminal_win, true)
-    terminal_win = nil
-  elseif terminal_buf and vim.api.nvim_buf_is_valid(terminal_buf) then
-    -- Terminal buffer exists but window not open, open it
-    vim.cmd("split")
-    terminal_win = vim.api.nvim_get_current_win()
-    vim.api.nvim_win_set_buf(terminal_win, terminal_buf)
-  else
-    -- No terminal exists, create new
-    vim.cmd("split | terminal")
-    terminal_buf = vim.api.nvim_get_current_buf()
-    terminal_win = vim.api.nvim_get_current_win()
-  end
-end, { desc = "Toggle terminal  in split" })
-
 -- run current file by <leader>rr
 vim.keymap.set("n", "<leader>rr", RunCurrentFile, { noremap = true, silent = true })
 
--- by using <leader>jj malual formatting.
-vim.keymap.set("n", "<leader>jj", function()
+-- by using <leader>jf malual formatting.
+vim.keymap.set("n", "<leader>jf", function()
   require("conform").format({ async = true, lsp_fallback = true, timeout_ms = 1000 })
-  vim.notify("For fomatted with <leader>jj 🚀", vim.log.levels.INFO)
+  vim.notify("Fomatted", vim.log.levels.INFO)
 end, { desc = "Manual format" })
-
--- Run single file
-vim.keymap.set("n", "<leader>rj", function()
-  local filename = vim.fn.expand("%:p:r")
-  vim.cmd("split | terminal javac % && java " .. filename)
-end, { desc = "Run Java File" })
