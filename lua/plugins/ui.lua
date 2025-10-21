f8ae0b9c12b73e0b5cdb73db541ed173a453cf3c
Bimbok <bimbokmkj@gmail.com>
2025-10-21 10:54:27 +0530

    redesign

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,4 +17,113 @@ return {
     event = "VeryLazy", -- Load it after startup
     -- opts = {} -- You can add configuration options here if you want
   },
+  { "github/copilot.vim" },
+  {
+    "karb94/neoscroll.nvim",
+    opts = {
+      -- All these keys are optional, but this is a nice default
+      easing_function = "quadratic", -- "linear", "quadratic", "cubic", "quintic"
+      hide_cursor = true,
+      stop_eof = true,
+    },
+  },
+  {
+    {
+      "rcarriga/nvim-notify",
+      config = function()
+        vim.notify = require("notify")
+        require("notify").setup({
+          stages = "slide", -- or "slide", "static"
+          timeout = 3000,
+          background_colour = "#000000",
+        })
+      end,
+    },
+    {
+      "folke/noice.nvim",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+        "rcarriga/nvim-notify",
+      },
+      event = "VeryLazy",
+      config = function()
+        require("noice").setup({
+          lsp = {
+            progress = {
+              enabled = true,
+            },
+            signature = {
+              enabled = true,
+            },
+            hover = {
+              enabled = true,
+            },
+          },
+          messages = {
+            enabled = true,
+          },
+          notify = {
+            enabled = true,
+          },
+          views = {
+            mini = {
+              win_options = {
+                winblend = 10,
+              },
+            },
+          },
+          cmdline = {
+            enabled = true,
+            view = "cmdline_popup",
+          },
+          popupmenu = {
+            enabled = true,
+          },
+        })
+      end,
+    },
+  },
+  {
+    {
+      "akinsho/toggleterm.nvim",
+      version = "*",
+      config = true,
+    },
+  },
+  {
+    "pocco81/auto-save.nvim",
+    trigger_event = { "InsertLeave", "TextChanged" },
+    opts = {
+      enabled = true,
+      execution_message = {
+        message = function()
+          return ("AutoSave: saved at " .. vim.fn.strftime("%H:%M:%S"))
+        end,
+        dim = 0.18,
+        cleaning_interval = 1250,
+      },
+      debounce_delay = 135,
+      condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+
+        if fn.getbufvar(buf, "&modifiable") == 1 and utils.not_in(fn.getbufvar(buf, "&filetype"), {}) then
+          return true -- met condition(s), can save
+        end
+        return false -- can't save
+      end,
+    },
+  },
+  {
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        "nvim-tree/nvim-web-devicons", -- optional, but recommended
+      },
+      lazy = false, -- neo-tree will lazily load itself
+    },
+  },
 }
