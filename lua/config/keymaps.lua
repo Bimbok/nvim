9695e5c76bc00eb857c4b19223743fd95e3f3b42
Bimbok <bimbokmkj@gmail.com>
2025-10-21 13:02:40 +0530

    floating-terminal

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,10 +2,15 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Toggle Neo-tree
+-- toggle neo-tree
 vim.keymap.set("n", "<leader>ee", function()
   vim.cmd("Neotree toggle right")
 end, { desc = "Toggle Neo-tree" })
+
+-- floating terminal
+vim.keymap.set("n", "<leader>'", function()
+  vim.cmd("ToggleTerm direction=float")
+end, { desc = "Toggle floating terminal" })
 
 -- run current file by <leader>rr
 vim.keymap.set("n", "<leader>rr", RunCurrentFile, { noremap = true, silent = true })
