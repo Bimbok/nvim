b8bb1d882f9ea5bec9f0b861d4bf62ef125febe4
Bimbok <bimbokmkj@gmail.com>
2026-03-21 19:32:28 +0530

    feat: csv-support

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -186,7 +186,7 @@ return {
         contrast = "soft", -- can be "hard", "soft" or empty string
         palette_overrides = {},
         overrides = {},
-        dim_inactive = true,
+        dim_inactive = false,
         transparent_mode = true,
       })
       vim.cmd([[colorscheme gruvbox]])
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -19,6 +19,33 @@ return {
       vim.keymap.set("n", "<leader>mt", require("render-markdown").toggle, { desc = "Toggle Render Markdown" })
     end,
   },
+  -- 1. The Viewer (Adds alignment and borders)
+  {
+    "hat0uma/csvview.nvim",
+    ft = { "csv", "tsv" },
+    opts = {
+      parser = { comments = { "#", "//" } },
+      view = {
+        display_mode = "border", -- Spreadsheet look
+      },
+    },
+    config = function(_, opts)
+      local csvview = require("csvview")
+      csvview.setup(opts)
+      csvview.enable()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "csv", "tsv" },
+        callback = function()
+          csvview.enable()
+        end,
+      })
+    end,
+  },
+  {
+    "mechatroner/rainbow_csv",
+    ft = { "csv", "tsv" },
+  },
+
   {
     "neovim/nvim-lspconfig",
     opts = {
